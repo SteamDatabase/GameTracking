d756eb654c4715192213b8171e7eff08dfe896ab
Steam <freeman@steamdb.info>
2015-10-29 20:14:20 +0000

    A  373301/game/dota/scripts/vscripts/animation/heroes/zeus/zeus.lua, M  BuildbotPaths/dota/libclient.txt, M  Protobufs/dota/dota_gcmessages_client.proto, M  Strings/dota/libclient.txt, M  Strings/dota/libserver.txt, M  Symbols/dota/libclient.txt

---
+++ 373301/game/dota/scripts/vscripts/animation/heroes/zeus/zeus.lua
@@ -0,0 +1,76 @@
+-- WeightList
+
+
+-- AsLookLayer
+model:CreateSequence(
+	{
+		name = "@zeus_turns_lookFrame_0",
+		snap = true,
+		delta = true,
+		hidden = true,
+		cmds = {
+			{ cmd = "fetchframe", sequence = "@zeus_turns", frame = 0, dst = 0 },
+			{ cmd = "fetchframe", sequence = "@zeus_turns", frame = 1, dst = 1 },
+			{ cmd = "subtract", dst = 0, src = 1 }
+		}
+	}
+)
+
+model:CreateSequence(
+	{
+		name = "@zeus_turns_lookFrame_1",
+		snap = true,
+		delta = true,
+		hidden = true,
+		cmds = {
+			{ cmd = "fetchframe", sequence = "@zeus_turns", frame = 1, dst = 0 },
+			{ cmd = "fetchframe", sequence = "@zeus_turns", frame = 1, dst = 1 },
+			{ cmd = "subtract", dst = 0, src = 1 }
+		}
+	}
+)
+
+model:CreateSequence(
+	{
+		name = "@zeus_turns_lookFrame_2",
+		snap = true,
+		delta = true,
+		hidden = true,
+		cmds = {
+			{ cmd = "fetchframe", sequence = "@zeus_turns", frame = 2, dst = 0 },
+			{ cmd = "fetchframe", sequence = "@zeus_turns", frame = 1, dst = 1 },
+			{ cmd = "subtract", dst = 0, src = 1 }
+		}
+	}
+)
+
+model:CreateSequence(
+	{
+		name = "zeus_turns",
+		delta = true,
+		poseParamX = model:CreatePoseParameter( "turn", -1, 1, 0, false ),
+		sequences = {
+			{ "@zeus_turns_lookFrame_0", "@zeus_turns_lookFrame_1", "@zeus_turns_lookFrame_2" }
+		}
+	}
+)
+
+
+-- AsTurningRun
+
+
+
+model:CreateSequence(
+	{
+		name = "zeus_run",
+		sequences = {
+			{ "@zeus_run" }
+		},
+		addlayer = {
+			"zeus_turns"
+		},
+		activities = {
+			{ name = "ACT_DOTA_RUN", weight = 1 }
+		}
+	}
+)
