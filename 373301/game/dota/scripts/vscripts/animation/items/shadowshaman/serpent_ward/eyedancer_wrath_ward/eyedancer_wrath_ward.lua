73cdd34ac5948b51db7c1f68ae7a95fa0eeea214
Steam <freeman@steamdb.info>
2015-10-29 20:11:39 +0000

    M  373301/game/bin/built_from_cl.txt, M  373301/game/dota/resource/items_english.txt, A  373301/game/dota/scripts/vscripts/animation/items/shadowshaman/serpent_ward/eyedancer_wrath_ward/eyedancer_wrath_ward.lua

---
+++ 373301/game/dota/scripts/vscripts/animation/items/shadowshaman/serpent_ward/eyedancer_wrath_ward/eyedancer_wrath_ward.lua
@@ -0,0 +1,27 @@
+-- Workshop Importer [serpent_ward_idle]: Multiblend sequence
+model:CreateSequence(
+	{
+		name = "ward_idle_multi",
+		looping = true,
+		poseParamX = model:CreatePoseParameter( "idle_aim", -1, 1, 0, false ),
+		sequences = {
+			{ "backleftidle", "leftidle", "idle", "rightidle", "backrightidle" }
+		},
+		activities = {
+			{ name = "ACT_DOTA_IDLE", weight = 1 }
+		}
+	}
+)
+-- Workshop Importer [serpent_ward_attack]: Multiblend sequence
+model:CreateSequence(
+	{
+		name = "ward_attack_multi",
+		poseParamX = model:CreatePoseParameter( "attack_aim", -1, 1, 0, false ),
+		sequences = {
+			{ "backleftattack", "leftattack", "attack", "rightattack", "backrightattack" }
+		},
+		activities = {
+			{ name = "ACT_DOTA_ATTACK", weight = 1 }
+		}
+	}
+)
