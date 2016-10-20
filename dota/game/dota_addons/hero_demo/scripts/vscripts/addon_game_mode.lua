bee75ff4486a3fbc9d4372682a3dfa3261516f1c
Steam <freeman@steamdb.info>
2016-10-20 23:12:03 +0000

    M  BuildbotPaths/dota/libclient.txt, M  Protobufs/dota/dota_gcmessages_client.proto, M  Strings/dota/libclient.txt, M  Strings/dota/libengine2.txt, M  dota/game/bin/built_from_cl.txt, M  dota/game/dota/pak01_dir.txt, A  dota/game/dota/pak01_dir/panorama/layout/battle_pass/fall2016/comp_boston2016_invites.xml, M  dota/game/dota/pak01_dir/panorama/styles/battle_pass/fall2016/bp_fall2016.css, A  dota/game/dota/pak01_dir/panorama/styles/battle_pass/fall2016/comp_boston2016_invites.css, M  dota/game/dota/pak01_dir/scripts/items/items_game.txt, M  dota/game/dota/pak01_dir/scripts/tournaments/challenge_templates.txt, M  dota/game/dota/panorama/localization/dota_english.txt, M  dota/game/dota_addons/hero_demo/scripts/vscripts/addon_game_mode.lua

--- dota/game/dota_addons/hero_demo/scripts/vscripts/addon_game_mode.lua
+++ dota/game/dota_addons/hero_demo/scripts/vscripts/addon_game_mode.lua
@@ -93,6 +93,7 @@ function CHeroDemo:InitGameMode()
 	SendToServerConsole( "dota_hero_god_mode 0" )
 	SendToServerConsole( "dota_ability_debug 0" )
 	SendToServerConsole( "dota_creeps_no_spawning 0" )
+	SendToServerConsole( "dota_easybuy 1" )
 	--SendToServerConsole( "dota_bot_mode 1" )
 
 	self.m_sHeroSelection = sHeroSelection -- this seems redundant, but events.lua doesn't seem to know about sHeroSelection
