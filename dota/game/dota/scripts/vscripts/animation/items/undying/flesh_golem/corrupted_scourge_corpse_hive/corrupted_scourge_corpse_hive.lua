e139c6b20ba8ee2eb33090e94ddb6b806eef3652
Steam <freeman@steamdb.info>
2016-11-18 03:09:22 +0000

    M  Strings/dota/libclient.txt, M  Strings/dota/libserver.txt, A  dota/content/dota/materials/models/heroes/visage/visage_diffusewarp.txt, A  dota/content/dota/materials/models/heroes/visage/visage_familiar_glow_detail.txt, M  dota/game/bin/built_from_cl.txt, M  dota/game/dota/pak01_dir.txt, M  dota/game/dota/pak01_dir/panorama/layout/battle_pass/fall2016/bp_fall2016_rewards.xml, M  dota/game/dota/pak01_dir/panorama/layout/frontpage_contents.xml, A  dota/game/dota/pak01_dir/panorama/layout/popups/splash_ads/popup_fall2016_treasure_3.xml, M  dota/game/dota/pak01_dir/panorama/styles/battle_pass/fall2016/bp_fall2016_rewards.css, M  dota/game/dota/pak01_dir/panorama/styles/frontpage_contents.css, A  dota/game/dota/pak01_dir/panorama/styles/popups/splash_ads/popup_fall2016_treasure_3.css, M  dota/game/dota/pak01_dir/scripts/items/items_game.txt, M  dota/game/dota/pak01_dir/scripts/itemtest_manifest.txt, M  dota/game/dota/pak01_dir/scripts/npc/npc_heroes.txt, M  dota/game/dota/pak01_dir/scripts/tournaments/challenge_templates.txt, M  dota/game/dota/pak01_dir/scripts/workshop_import_templates/template_pugna_ward.txt, A  dota/game/dota/pak01_dir/scripts/workshop_import_templates/template_visage_familiar.txt, M  dota/game/dota/pak01_dir/scripts/workshop_import_templates/templates.txt, M  dota/game/dota/panorama/localization/dota_english.txt, M  dota/game/dota/resource/dota_english.txt, M  dota/game/dota/resource/items_english.txt, M  dota/game/dota/scripts/vscripts/animation/items/undying/flesh_golem/corrupted_scourge_corpse_hive/corrupted_scourge_corpse_hive.lua

--- dota/game/dota/scripts/vscripts/animation/items/undying/flesh_golem/corrupted_scourge_corpse_hive/corrupted_scourge_corpse_hive.lua
+++ dota/game/dota/scripts/vscripts/animation/items/undying/flesh_golem/corrupted_scourge_corpse_hive/corrupted_scourge_corpse_hive.lua
@@ -1,108 +1,260 @@
--- Workshop Importer [run_anims]: Pose sequence
-model:CreateSequence(
-		  {
-				name = "turn_center",
-				delta = true,
-				fadeInTime = 0,
-				fadeOutTime = 0,
-				fps = 30,
-				framerangesequence = "center_pose",
-				cmds = {
-					 { cmd = "sequence", sequence = "center_pose", dst = 1 },
-					 { cmd = "fetchframe", sequence = "center_pose", frame = 0, dst = 2 },
-					 { cmd = "subtract", dst = 1, src = 2 },
-					 { cmd = "slerp", dst = 0, src = 1 }
-				}
-		  }
+--============ Copyright (c) Valve Corporation, All rights reserved. ==========
+--
+-- source1import auto-generated animation script
+-- local changes will be overwritten if source1import if run again on this asset
+--
+-- mdl: models\heroes\undying\undying_flesh_golem.mdl
+--
+--=============================================================================
+
+model:CreateWeightlist(
+	"upperBod",
+	{
+		{ "spine1", 1 }
+	}
 )
--- Workshop Importer [run_anims]: Pose sequence
-model:CreateSequence(
-		  {
-				name = "turn_left",
-				delta = true,
-				fadeInTime = 0,
-				fadeOutTime = 0,
-				fps = 30,
-				framerangesequence = "left_pose",
-				cmds = {
-					 { cmd = "sequence", sequence = "left_pose", dst = 1 },
-					 { cmd = "fetchframe", sequence = "center_pose", frame = 0, dst = 2 },
-					 { cmd = "subtract", dst = 1, src = 2 },
-					 { cmd = "slerp", dst = 0, src = 1 }
-				}
-		  }
-)
--- Workshop Importer [run_anims]: Pose sequence
-model:CreateSequence(
-		  {
-				name = "turn_right",
-				delta = true,
-				fadeInTime = 0,
-				fadeOutTime = 0,
-				fps = 30,
-				framerangesequence = "right_pose",
-				cmds = {
-					 { cmd = "sequence", sequence = "right_pose", dst = 1 },
-					 { cmd = "fetchframe", sequence = "center_pose", frame = 0, dst = 2 },
-					 { cmd = "subtract", dst = 1, src = 2 },
-					 { cmd = "slerp", dst = 0, src = 1 }
-				}
-		  }
-)
--- Workshop Importer [run_anims]: Turns sequence
-model:CreateSequence(
-	  {
-			name = "turns",
-			delta = true,
-		    fadeInTime = 0.2,
-			fadeOutTime = 0.2,
-			poseParamX = model:CreatePoseParameter( "turn", -1, 1, 0, false ),
-			sequences = {
-				 { "turn_left", "turn_center", "turn_right" }
-			}
-	  }
-)
--- Workshop Importer [run_anims]: Run sequence
+
+
+
+-- AsLookLayer
 model:CreateSequence(
 	{
-		name = "run",
-		looping = true,
-		sequences = {
-			{ "@run" }
-		},
-		addlayer = { "turns" },
-		activities = {
-				{ name = "ACT_DOTA_RUN", weight = 1 }
+		name = "@flesh_golem_turns_lookFrame_0",
+		snap = true,
+		delta = true,
+		hidden = true,
+		cmds = {
+			{ cmd = "fetchframe", sequence = "@flesh_golem_turns", frame = 0, dst = 0 },
+			{ cmd = "fetchframe", sequence = "@flesh_golem_turns", frame = 1, dst = 1 },
+			{ cmd = "subtract", dst = 0, src = 1 }
 		}
 	}
 )
--- Workshop Importer [run_anims]: Run sequence
+
 model:CreateSequence(
 	{
-		name = "run_injured",
-		looping = true,
-		sequences = {
-			{ "@run_injured" }
-		},
-		addlayer = { "turns" },
-		activities = {
-				{ name = "ACT_DOTA_RUN", weight = 1 },
-				{ name = "injured", weight = 1 }
+		name = "@flesh_golem_turns_lookFrame_1",
+		snap = true,
+		delta = true,
+		hidden = true,
+		cmds = {
+			{ cmd = "fetchframe", sequence = "@flesh_golem_turns", frame = 1, dst = 0 },
+			{ cmd = "fetchframe", sequence = "@flesh_golem_turns", frame = 1, dst = 1 },
+			{ cmd = "subtract", dst = 0, src = 1 }
 		}
 	}
 )
--- Workshop Importer [run_anims]: Run sequence
+
 model:CreateSequence(
 	{
-		name = "run_haste",
-		looping = true,
+		name = "@flesh_golem_turns_lookFrame_2",
+		snap = true,
+		delta = true,
+		hidden = true,
+		cmds = {
+			{ cmd = "fetchframe", sequence = "@flesh_golem_turns", frame = 2, dst = 0 },
+			{ cmd = "fetchframe", sequence = "@flesh_golem_turns", frame = 1, dst = 1 },
+			{ cmd = "subtract", dst = 0, src = 1 }
+		}
+	}
+)
+
+model:CreateSequence(
+	{
+		name = "flesh_golem_turns",
+		delta = true,
+		poseParamX = model:CreatePoseParameter( "turn", -1, 1, 0, false ),
 		sequences = {
-			{ "@run_haste" }
+			{ "@flesh_golem_turns_lookFrame_0", "@flesh_golem_turns_lookFrame_1", "@flesh_golem_turns_lookFrame_2" }
+		}
+	}
+)
+
+-- AsTurningRun
+
+model:CreateSequence(
+	{
+		name = "flesh_golem_run",
+		sequences = {
+			{ "@flesh_golem_run" }
 		},
-		addlayer = { "turns" },
+		addlayer = {
+			"flesh_golem_turns"
+		},
 		activities = {
-				{ name = "ACT_DOTA_RUN", weight = 1 },
-				{ name = "haste", weight = 1 }
+			{ name = "ACT_DOTA_RUN", weight = 1 }
+		}
+	}
+)
+
+
+-- AsTurningRun
+
+model:CreateSequence(
+	{
+		name = "flesh_golem_run_injured",
+		sequences = {
+			{ "@flesh_golem_run_injured" }
+		},
+		addlayer = {
+			"flesh_golem_turns"
+		},
+		activities = {
+			{ name = "ACT_DOTA_RUN", weight = 1 },
+			{ name = "injured", weight = 1 }
+		}
+	}
+)
+
+
+-- AsTurningRun
+
+model:CreateSequence(
+	{
+		name = "flesh_golem_run_haste",
+		sequences = {
+			{ "@flesh_golem_run_haste" }
+		},
+		addlayer = {
+			"flesh_golem_turns"
+		},
+		activities = {
+			{ name = "ACT_DOTA_RUN", weight = 1 },
+			{ name = "haste", weight = 1 }
+		}
+	}
+)
+
+
+model:CreateSequence(
+	{
+		name = "flesh_golem_cast1_decay",
+		framerangesequence = "@decay",
+		cmds = {
+			{ cmd = "sequence", sequence = "@decay", dst = 1 },
+			{ cmd = "fetchframe", sequence = "@decay", frame = 0, dst = 2 },
+			{ cmd = "subtract", dst = 1, src = 2 },
+			{ cmd = "add", dst = 0, src = 1 }
+		},
+		activities = {
+			{ name = "ACT_DOTA_UNDYING_DECAY", weight = 1 }
+		}
+	}
+)
+
+
+model:CreateSequence(
+	{
+		name = "flesh_golem_cast1_decay_injured",
+		framerangesequence = "@flesh_golem_cast1_decay_injured",
+		cmds = {
+			{ cmd = "sequence", sequence = "@flesh_golem_cast1_decay_injured", dst = 1 },
+			{ cmd = "fetchframe", sequence = "@flesh_golem_cast1_decay_injured", frame = 0, dst = 2 },
+			{ cmd = "subtract", dst = 1, src = 2 },
+			{ cmd = "add", dst = 0, src = 1 }
+		},
+		activities = {
+			{ name = "ACT_DOTA_UNDYING_DECAY", weight = 1 },
+			{ name = "injured", weight = 1 }
+		}
+	}
+)
+
+
+model:CreateSequence(
+	{
+		name = "flesh_golem_cast_2_soul_rip",
+		framerangesequence = "@flesh_golem_cast_2_soul_rip",
+		cmds = {
+			{ cmd = "sequence", sequence = "@flesh_golem_cast_2_soul_rip", dst = 1 },
+			{ cmd = "fetchframe", sequence = "@flesh_golem_cast_2_soul_rip", frame = 0, dst = 2 },
+			{ cmd = "subtract", dst = 1, src = 2 },
+			{ cmd = "add", dst = 0, src = 1 }
+		},
+		activities = {
+			{ name = "ACT_DOTA_UNDYING_SOUL_RIP", weight = 1 }
+		}
+	}
+)
+
+
+model:CreateSequence(
+	{
+		name = "flesh_golem_cast_2_soul_rip_injured",
+		framerangesequence = "@flesh_golem_cast_2_soul_rip_injured",
+		cmds = {
+			{ cmd = "sequence", sequence = "@flesh_golem_cast_2_soul_rip_injured", dst = 1 },
+			{ cmd = "fetchframe", sequence = "@flesh_golem_cast_2_soul_rip_injured", frame = 0, dst = 2 },
+			{ cmd = "subtract", dst = 1, src = 2 },
+			{ cmd = "add", dst = 0, src = 1 }
+		},
+		activities = {
+			{ name = "ACT_DOTA_UNDYING_SOUL_RIP", weight = 1 },
+			{ name = "injured", weight = 1 }
+		}
+	}
+)
+
+
+model:CreateSequence(
+	{
+		name = "flesh_golem_cast_3_tombstone",
+		framerangesequence = "@flesh_golem_cast_3_tombstone",
+		cmds = {
+			{ cmd = "sequence", sequence = "@flesh_golem_cast_3_tombstone", dst = 1 },
+			{ cmd = "fetchframe", sequence = "@flesh_golem_cast_3_tombstone", frame = 0, dst = 2 },
+			{ cmd = "subtract", dst = 1, src = 2 },
+			{ cmd = "add", dst = 0, src = 1 }
+		},
+		activities = {
+			{ name = "ACT_DOTA_UNDYING_TOMBSTONE", weight = 1 }
+		}
+	}
+)
+
+
+model:CreateSequence(
+	{
+		name = "flesh_golem_cast_3_tombstone_injured",
+		framerangesequence = "@flesh_golem_cast_3_tombstone_injured",
+		cmds = {
+			{ cmd = "sequence", sequence = "@flesh_golem_cast_3_tombstone_injured", dst = 1 },
+			{ cmd = "fetchframe", sequence = "@flesh_golem_cast_3_tombstone_injured", frame = 0, dst = 2 },
+			{ cmd = "subtract", dst = 1, src = 2 },
+			{ cmd = "add", dst = 0, src = 1 }
+		},
+		activities = {
+			{ name = "ACT_DOTA_UNDYING_TOMBSTONE", weight = 1 },
+			{ name = "injured", weight = 1 }
+		}
+	}
+)
+
+
+model:CreateSequence(
+	{
+		name = "flesh_golem_spawn",
+		sequences = {
+			{ "@flesh_golem_spawn" }
+		},
+		weightlist = "upperBod",
+		activities = {
+			{ name = "ACT_DOTA_SPAWN", weight = 1 }
+		}
+	}
+)
+
+
+model:CreateSequence(
+	{
+		name = "flesh_golem_spawn_injured",
+		sequences = {
+			{ "@flesh_golem_spawn_injured" }
+		},
+		weightlist = "upperBod",
+		activities = {
+			{ name = "ACT_DOTA_SPAWN", weight = 1 },
+			{ name = "injured", weight = 1 }
 		}
 	}
 )
