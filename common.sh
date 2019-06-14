bda480becec3060ef48dbe5b3fdcd049bd38a1cd
Steam <freeman@steamdb.info>
2019-06-14 16:26:46 +0000

    ↑ | 2 files | M common.sh,  M depots_mapping.json

--- common.sh
+++ common.sh
@@ -45,7 +45,7 @@ ProcessVPK ()
 		
 		"$(dirname "${BASH_SOURCE[0]}")/.support/vpktool" "$file" > "${file%.*}.txt"
 		
-		dotnet /home/steamdb/ValveResourceFormat/Decompiler/bin/Release/netcoreapp2.0/Decompiler.dll --input "$file" --output "$(echo "$file" | sed -e 's/\.vpk$/\//g')" --vpk_cache --vpk_decompile --vpk_extensions "vxml_c,vjs_c,vcss_c,vsndevts_c,vpcf_c,txt,cfg,res,pop,png,jpg,gif"
+		dotnet /home/steamdb/ValveResourceFormat/Decompiler/bin/Release/netcoreapp2.0/Decompiler.dll --input "$file" --output "$(echo "$file" | sed -e 's/\.vpk$/\//g')" --vpk_cache --vpk_decompile --vpk_extensions "vxml_c,vjs_c,vcss_c,vsndevts_c,vpcf_c,json,txt,cfg,res,pop,png,jpg,gif"
 	done <   <(find . -type f -name "*_dir.vpk" -print0)
 }
 
