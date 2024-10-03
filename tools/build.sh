4ecb044d00e07e3eb0495e03a870e0a70f869cef
Steam <freeman@steamdb.info>
2024-10-03 09:36:44 +0000

    update dumper

--- tools/build.sh
+++ tools/build.sh
@@ -8,12 +8,12 @@ git submodule update --remote --merge
 
 # VRF
 cd ValveResourceFormat
-dotnet clean --configuration Release Decompiler/Decompiler.csproj
+#dotnet clean --configuration Release Decompiler/Decompiler.csproj
 dotnet publish --configuration Release -p:PublishSingleFile=true --runtime linux-x64 --self-contained Decompiler/Decompiler.csproj
 
 # ProtobufDumper
 cd ../SteamKit
-dotnet clean --configuration Release Resources/ProtobufDumper/ProtobufDumper/ProtobufDumper.csproj
+#dotnet clean --configuration Release Resources/ProtobufDumper/ProtobufDumper/ProtobufDumper.csproj
 dotnet publish --configuration Release -p:PublishSingleFile=true --runtime linux-x64 --self-contained Resources/ProtobufDumper/ProtobufDumper/ProtobufDumper.csproj
 
 # Strings
@@ -22,12 +22,12 @@ go build
 
 # Dumper
 cd ../DumpSource2
-git submodule update # git sucks
+git submodule update --init
 [ -d build ] && rm -r build
 mkdir build
 cd build
 cmake ..
-make -j 4
+cmake --build . --parallel 4
 cd ../
 
 # Verify
--- tools/dump_source2.sh
+++ tools/dump_source2.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 set -euo pipefail
 
-if [[ $# -lt 1 ]]; then
-	echo "Pass in game name"
+if [[ $# -lt 2 ]]; then
+	echo "$(basename "$0") <game folder name> <mod name>"
 	exit 1
 fi
 
 TOOLS_DIR="$(dirname "$(realpath -s "${BASH_SOURCE[0]}")")"
-DUMPER_PATH="$TOOLS_DIR/DumpSource2/build/CS2Dumper"
+DUMPER_PATH="$TOOLS_DIR/DumpSource2/build/DumpSource2"
 
 cd "$TOOLS_DIR" || exit 1
 
-GAME_DIR="$(realpath "../$1/game/bin/linuxsteamrt64/")"
+MOD_NAME="$2"
+CORE_DIR="$(realpath "../$1/game/bin/linuxsteamrt64/")"
+DUMP_DIR="$(realpath "../$1/DumpSource2/")"
 
-cd "$GAME_DIR" || exit 1
+cd "$CORE_DIR" || exit 1
 
 # Create a stub of libvideo to avoid installing video dependencies
 python3 "$TOOLS_DIR/implib/implib-gen.py" --no-dlopen libvideo.so
@@ -21,7 +23,6 @@ mv libvideo.so libvideo.so.original
 gcc -DIMPLIB_EXPORT_SHIMS=1 -g -fPIC -shared libvideo.so.tramp.S libvideo.so.init.c -ldl -o libvideo.so
 rm libvideo.so.tramp.S libvideo.so.init.c
 
-# todo move cs2dumper
-LD_LIBRARY_PATH="$GAME_DIR" timeout 2m "$DUMPER_PATH" || true
+LD_LIBRARY_PATH="$CORE_DIR" timeout 2m "$DUMPER_PATH" "$MOD_NAME" "$DUMP_DIR" || true
 
 mv libvideo.so.original libvideo.so
