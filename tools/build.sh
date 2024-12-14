7c87455a021a7a67b278cbab4e8e36403c334cc8
Steam <freeman@steamdb.info>
2024-12-14 18:32:23 +0000

    update s2v to remove version from dumps

--- tools/build.sh
+++ tools/build.sh
@@ -9,7 +9,7 @@ git submodule update --remote --merge --init
 # VRF
 cd ValveResourceFormat
 #dotnet clean --configuration Release Decompiler/Decompiler.csproj
-dotnet publish --configuration Release -p:PublishSingleFile=true --runtime linux-x64 --self-contained Decompiler/Decompiler.csproj
+dotnet publish --configuration Release -p:PublishSingleFile=true --runtime linux-x64 --self-contained -p:DefineConstants=VRF_NO_GENERATOR_VERSION Decompiler/Decompiler.csproj
 
 # ProtobufDumper
 cd ../SteamKit
